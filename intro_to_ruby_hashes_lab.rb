cc9af5c9f7dcb83a20781b160947443733893347
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-13 13:55:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,12 +26,12 @@ hash[key]
 end
 
 
-def update_counting_hash(hash, key)
+def update_counting_hash(hash, :key)
 
   if
-    hash[key]
-    hash[key]=hash[key]+1
+    hash[:key]
+    hash[:key]=hash[:key]+1
   else
-    hash[key]=1
+    hash[:key]=1
 end
 end
